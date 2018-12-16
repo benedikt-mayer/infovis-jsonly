c4e697fa3a87f58678ced4b689dd760d6353e14d
Benedikt Mayer <benedikt_mayer@outlook.de>
2018-12-16 12:33:23 +0000

    cleaned up server script

--- server.py
+++ server.py
@@ -13,7 +13,6 @@ app.config['MYSQL_DATABASE_DB'] = 'newspapers'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-# app.config['MYSQL_DATABASE_CHARSET'] = 'utf8'
 mysql.init_app(app)
 
 
@@ -23,7 +22,6 @@ class Title(Resource):
         conn = mysql.connect()
         cursor = conn.cursor()
 
-        # cursor.execute("SELECT title, description FROM newspapers.documents")
         query = ("SELECT doc.id, token.token "
                  "FROM newspapers.tokens AS token "
                  "INNER JOIN newspapers.documents_tokens AS doc_token "
@@ -32,28 +30,16 @@ class Title(Resource):
                  "AND doc_token.document_id = doc.id "
                  "LIMIT 0, 200")
         cursor.execute(query)
-#         cursor.execute("""SELECT doc.id, token.token
-# FROM
-#     newspapers.tokens AS token
-#         INNER JOIN
-#     newspapers.documents_tokens AS doc_token
-#         INNER JOIN
-#     newspapers.documents AS doc
-# WHERE
-#     token.id = doc_token.token_id
-#         AND doc_token.document_id = doc.id
-# """)
         articles = dict()
         data = cursor.fetchall()
         for item in data:
             if len(articles) < item[0]:
-                articles[item[0]] = item[1]  # .decode("utf-8")
+                articles[item[0]] = item[1]
             else:
                 articles[item[0]] = articles[item[0]] + \
-                    " " + item[1]  # .decode("utf-8")
+                    " " + item[1]
         print(articles[1])
         return articles[1]
-        # return data
 
 
 api.add_resource(Title, '/')  # Route_1
